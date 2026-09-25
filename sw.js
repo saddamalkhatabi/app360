d99cd376688669dc1c5c543c8f36f42a0d37bbbd
saddamalkhatabi <sadamalkhatabi@gmail.com>
2026-09-25 05:51:30 +0300

    Refresh portal cache for sensory cover fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='app360-portal-v32';
+var CACHE='app360-portal-v33';
 var CORE=[
   './','./index.html',
   './assets/css/lab360.css','./assets/css/legacy-compat.css','./assets/css/home-showcase-v31.css',
